5dfc83386ed9d58e73660b3d71a9498ac54da7db
Alexandre Cuva <alexandre.cuva@gmail.com>
2024-03-27 16:45:13 +0100

    refactor(all): Extract constant results

--- src/HOTPaperScisorRock.Core/Domain/ScissorsRockPaperUseCase.cs
+++ src/HOTPaperScisorRock.Core/Domain/ScissorsRockPaperUseCase.cs
@@ -2,16 +2,20 @@ namespace HOTPaperScisorRock.Core.Domain;
 
 public class ScissorsRockPaperUseCase
 {
+    public const string RockCrushesScissors = "Rock crushes Scissors";
+    public const string ScissorsCutsPaper = "Scissors cuts Paper";
+    public const string PaperCoversRock = "Paper covers Rock";
+
     public HandActionResponse Play(HandActionRequest request)
     {
         var response = request.Hand1 switch
         {
-            HandAction.Rock when request.Hand2 == HandAction.Scissors => GetHandActionResponse(1, 0, "Rock crushes Scissors"),
-            HandAction.Scissors when request.Hand2 == HandAction.Paper => GetHandActionResponse(1,0, "Scissors cuts Paper"),
-            HandAction.Paper when request.Hand2 == HandAction.Rock => GetHandActionResponse(1, 0, "Paper covers Rock"),
-            HandAction.Scissors when request.Hand2 == HandAction.Rock => GetHandActionResponse(0,1, "Rock crushes Scissors"),
-            HandAction.Paper when request.Hand2 == HandAction.Scissors => GetHandActionResponse(0,1,"Scissors cuts Paper"),
-            HandAction.Rock when request.Hand2 == HandAction.Paper => GetHandActionResponse(0,1,"Paper covers Rock"),
+            HandAction.Rock when request.Hand2 == HandAction.Scissors => GetHandActionResponse(1, 0, RockCrushesScissors),
+            HandAction.Scissors when request.Hand2 == HandAction.Paper => GetHandActionResponse(1,0, ScissorsCutsPaper),
+            HandAction.Paper when request.Hand2 == HandAction.Rock => GetHandActionResponse(1, 0, PaperCoversRock),
+            HandAction.Scissors when request.Hand2 == HandAction.Rock => GetHandActionResponse(0,1, RockCrushesScissors),
+            HandAction.Paper when request.Hand2 == HandAction.Scissors => GetHandActionResponse(0,1,ScissorsCutsPaper),
+            HandAction.Rock when request.Hand2 == HandAction.Paper => GetHandActionResponse(0,1,PaperCoversRock),
             _ => GetHandActionResponse(0, 0,"Draw")
         };
         
